9bf10bd0499a0430e0ddb3da0f7289c948f78942
Mohamed Yamani <myamani@Mohameds-MacBook-Pro-2.local>
2024-07-04 11:13:16 +0100

    home screen body

--- lib/presentation/journeys/home/home_screen.dart
+++ lib/presentation/journeys/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutterforge/common/extensions%20/string_extensions.dart';
 import 'package:flutterforge/di/get_it.dart';
 import 'package:flutterforge/presentation/blocs/get_breeds_bloc/get_breeds_bloc.dart';
 import 'package:flutterforge/presentation/journeys/details/details_screen.dart';
+import 'package:flutterforge/presentation/journeys/home/home_screen_body.dart';
 import 'package:flutterforge/presentation/themes/toggle_theme.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -26,47 +27,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
         // drawer: MyNavigationDrawer(),
-        body: SafeArea(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            TranslationConstants.homePage.t(context),
-            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                  color: Theme.of(context).primaryColor,
-                ),
-          ),
-          Divider(
-            color: Theme.of(context).dividerColor,
-          ),
-          const ToggleTheme(),
-          // navigatin button to the next screen
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(RouteList.settings);
-            },
-            child: Text(
-              TranslationConstants.settings.t(context),
-            ),
-          ),
-          // DetailsScreen
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(RouteList.details,
-                  arguments: const DetailsArguments(
-                    title: 'Title 1',
-                    message: 'Messa ge',
-                  ));
-            },
-            child: Text(
-              TranslationConstants.details.t(context),
-            ),
-          ),
-        ],
-      ),
-    ));
+        body: HomeScreenBody());
   }
 }
