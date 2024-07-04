88d70fca573dc671bc9382b02f71097e6e3d545f
Mohamed Yamani <myamani@Mohameds-MacBook-Pro-2.local>
2024-07-04 09:32:07 +0100

    routes

---
+++ lib/common/constants/route_constants.dart
@@ -0,0 +1,16 @@
+class RouteList {
+  RouteList._();
+
+  static const String home = '/';
+  static const String detail = '/detail';
+  static const String favorite = '/favorite';
+  static const String setting = '/setting';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String profile = '/profile';
+  static const String editProfile = '/edit-profile';
+  static const String changePassword = '/change-password';
+  static const String splash = '/splash';
+  static const String onBoarding = '/on-boarding';
+  static const String search = '/search';
+}
